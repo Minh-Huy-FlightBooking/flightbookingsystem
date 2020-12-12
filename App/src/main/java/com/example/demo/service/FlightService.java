af88335e17bd8ddc65b026f292aab0dafe41d2d5
HikanVilan <almngoc@gmail.com>
2020-12-12 21:11:00 +0700

    flight service

--- App/src/main/java/com/example/demo/service/FlightService.java
+++ App/src/main/java/com/example/demo/service/FlightService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Flight;
+import com.example.demo.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlightService {
-
+    @Autowired
+    private FlightRepository flightRepository;
 }
