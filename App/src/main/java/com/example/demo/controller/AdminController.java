30da00268c250135e82c17c2d58e92892f77f6b6
Tran Vo Minh <tranvominh185@gmail.com>
2020-12-17 09:14:45 +0700

    Conflicts !!!! Resolve using Huy's Data !!! Some problems occured!

--- App/src/main/java/com/example/demo/controller/AdminController.java
+++ App/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Aircraft;
+import com.example.demo.entity.FlightRoute;
 import com.example.demo.service.AircraftService;
 import com.example.demo.service.AirportService;
 import com.example.demo.service.BrandService;
+import com.example.demo.service.FlightRouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+<<<<<<< HEAD
+=======
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+>>>>>>> 1344cc8def042ad17f0ce04cbd8a1f032d52ab99
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -45,6 +51,7 @@ public class AdminController {
         aircraftService.saveAircraft(aircraft);
         return "administration/adminHome";
     }
+<<<<<<< HEAD
 
     @RequestMapping(value = "/viewAircraft")
     public String viewAircraft (Model model){
@@ -67,4 +74,49 @@ public class AdminController {
         model.addAttribute("airports", airportService.getAllAirportsWithMapType());
         return "administration/aircraftAddition";
     }
+=======
+    // CRUD Flight Route
+    @Autowired
+    private FlightRouteService flightRouteService;
+    @RequestMapping(value = "/flightRouteList")
+    public String returnListFlightRoute(Model model){
+        model.addAttribute("flightRouteList", flightRouteService.getAllFlightRoute());
+        return "administration/flight-route-list";
+    }
+    @RequestMapping("/addFlightRoute")
+    public String gotoActionFlightRoutePage(Model model){
+        model.addAttribute("flightRoute", new FlightRoute());
+        model.addAttribute("originAirport", airportService.getAllAirports());
+        model.addAttribute("destinationAirport", airportService.getAllAirports());
+        model.addAttribute("message","add");
+        return "administration/flight-route-action";
+    }
+    @RequestMapping("/handlingSaveFlightRoute")
+    public String handleSaveFlightRoute(FlightRoute flightRoute, Model model, RedirectAttributes attributes){
+        if(flightRouteService.saveFlightRoute(flightRoute)){
+            attributes.addFlashAttribute("message","Successfully");
+        }else {
+            attributes.addFlashAttribute("message","failed");
+        }
+        return "redirect:flightRouteList";
+    }
+    @RequestMapping("/deleteFlightRoute")
+    public String deleteFlightRoute(@RequestParam("id") int id, Model model){
+        if(flightRouteService.deleteFlightRoute(id)){
+            model.addAttribute("message","Delete Successed");
+        }else {
+            model.addAttribute("message","Delete failed");
+        }
+        return "redirect:flightRouteList";
+    }
+    @RequestMapping("/editFlightRoute")
+    public String gotoActionFlightRoute(@RequestParam("id") int id, Model model){
+        model.addAttribute("flightRoute",flightRouteService.getFlightRouteById(id));
+        model.addAttribute("originAirport", airportService.getAllAirports());
+        model.addAttribute("destinationAirport", airportService.getAllAirports());
+        model.addAttribute("message","edit");
+        return "administration/flight-route-action";
+    }
+    // End Flight Route
+>>>>>>> 1344cc8def042ad17f0ce04cbd8a1f032d52ab99
 }
