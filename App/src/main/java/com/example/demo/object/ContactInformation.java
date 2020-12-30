aba7532777a103873866deb6d59714ac02f2a08e
HikanVilan <almngoc@gmail.com>
2020-12-30 15:17:35 +0700

    HI

---
+++ App/src/main/java/com/example/demo/object/ContactInformation.java
@@ -0,0 +1,34 @@
+package com.example.demo.object;
+
+public class ContactInformation {
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+
+    public ContactInformation() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
