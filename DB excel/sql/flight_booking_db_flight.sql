9c42c5e678d3015ee1ef3bdabe3a2aad2ce7d17c
HikanVilan <almngoc@gmail.com>
2020-12-30 15:48:41 +0700

    no message

---
+++ DB excel/sql/flight_booking_db_flight.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: flight_booking_db
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `flight`
+--
+
+DROP TABLE IF EXISTS `flight`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flight` (
+  `flight_id` int NOT NULL AUTO_INCREMENT,
+  `arrival_time` datetime(6) DEFAULT NULL,
+  `business_price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `departure_time` datetime(6) DEFAULT NULL,
+  `economy_price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `flight_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `aircraft_id` int DEFAULT NULL,
+  `route_id` int DEFAULT NULL,
+  PRIMARY KEY (`flight_id`),
+  KEY `FKmofq89ullrd4qk1hllnyf8pn5` (`aircraft_id`),
+  KEY `FKfh5cldb5ok61x8n4d0iv6bwi3` (`route_id`),
+  CONSTRAINT `FKfh5cldb5ok61x8n4d0iv6bwi3` FOREIGN KEY (`route_id`) REFERENCES `flight_route` (`route_id`),
+  CONSTRAINT `FKmofq89ullrd4qk1hllnyf8pn5` FOREIGN KEY (`aircraft_id`) REFERENCES `aircraft` (`aircraft_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flight`
+--
+
+LOCK TABLES `flight` WRITE;
+/*!40000 ALTER TABLE `flight` DISABLE KEYS */;
+INSERT INTO `flight` VALUES (3,'2020-12-20 00:00:00.000000','300','2020-12-19 00:00:00.000000','200','1',2,1),(4,'2020-12-31 00:00:00.000000','500','2020-12-28 00:00:00.000000','300','1',3,2),(6,'2020-12-29 00:00:00.000000','500','2020-12-28 12:30:00.000000','420','1',5,2),(7,'2021-01-03 00:00:00.000000','1000','2021-01-02 00:00:00.000000','500','1',6,65),(8,'2021-01-03 21:20:00.000000','900','2021-01-02 10:20:00.000000','400','1',7,65),(9,'2021-01-01 21:01:00.000000','440.0','2021-01-01 18:01:00.000000','212.0','On time',16,3),(10,'2021-01-04 21:09:00.000000','50.0','2021-01-04 16:08:00.000000','200.0','On time',2,9);
+/*!40000 ALTER TABLE `flight` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-30 15:40:15
